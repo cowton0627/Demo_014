44115f6b0e50b861ed2a8d6d347b11d8a38086d3
cowton0627 <cowton0517@gmail.com>
2021-07-25 21:22:21 +0800

    Initial Commit

---
+++ Demo_014/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Demo_014
+//
+//  Created by 鄭淳澧 on 2021/7/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
